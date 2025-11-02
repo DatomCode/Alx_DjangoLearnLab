e1c5e5c2b2d0b43d8c3a0f87d6e7b200ed7cd197
Datom-code <enochgbadebo007@gmail.com>
2025-11-02 15:38:39 +0100

    configuring admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,3 +5,14 @@ from .models import Book
 
 admin.ModelAdmin
 admin.site.register(Book)
+
+# @admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list
+    list_display = ('title', 'author', 'publication_year')
+
+    # Fields to filter by on the right sidebar
+    list_filter = ('author', 'publication_year')
+
+    # Fields to search by (search bar at the top)
+    search_fields = ('title', 'author')
