399a26eb9dabbf885cbc2cc281239402e1903a39
Datom-code <enochgbadebo007@gmail.com>
2025-11-06 14:58:23 +0100

    made changes in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .import views
+from views import lists_all_books
 from views import LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.lists_all_books, name= 'books'),
+    path('books/', lists_all_books.as_view(), name= 'books'),
     path('library/', LibraryDetailView.as_view(), name='library-details'),
 ]
