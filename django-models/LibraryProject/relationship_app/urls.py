8c25b85821e18c2175a4faf9162914d627adcc92
Datom-code <enochgbadebo007@gmail.com>
2025-11-14 12:53:46 +0100

    added correct url path

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,12 +11,16 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    
     path('admin_area/', views.admin_view, name='admin_view'),
     path('librarian_area/', views.librarian_view, name='librarian_view'),
     path('member_area/', views.member_view, name='member_view'),
-    path('books/add/', views.secured_book_add, name='book_add'),
-    path('books/edit/<int:pk>/', views.secured_book_edit, name='book_edit'),
-    path('books/delete/<int:pk>/', views.secured_book_delete, name='book_delete'),
+
+
+    path('add_book/', views.secured_book_add, name='book_add'),
+    path('edit_book/<int:pk>/', views.secured_book_edit, name='book_edit'),
+    path('delete_book/<int:pk>/', views.secured_book_delete, name='book_delete'),
 ]
 
 
