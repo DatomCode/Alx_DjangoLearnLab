e92cdb40f07f6b80faf413a12ffcefa6bab0faa9
Datom-code <enochgbadebo007@gmail.com>
2025-11-16 22:04:17 +0100

    3rd commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def book_list(request):
+    """
+    Displays a list of all books. Requires the 'can_view' permission.
+    """
+    # Task 1, Step 3 Requirement: The context variable 'books'
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'bookshelf/book_list.html', context)
