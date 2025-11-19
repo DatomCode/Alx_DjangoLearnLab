1eb02de1d5612210dc27f631fab0800f0a25b2cd
Datom-code <enochgbadebo007@gmail.com>
2025-11-19 01:27:46 +0100

    made changes to the url configuration

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,5 +2,5 @@ from .views import BookList
 from django.urls import path
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', BookList.as_view(), name='book-list'),  # Maps to the BookList view
 ]
