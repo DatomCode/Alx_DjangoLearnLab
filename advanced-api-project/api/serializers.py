f0230010b7448a77177e74ffabea56d9324b7b83
Datom-code <enochgbadebo007@gmail.com>
2025-11-30 18:42:14 +0100

    handled validation error for BookSerializer and added AuthorSerilaizer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from .models import Book, Author
+import datetime
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         name = Book
         fields = '__all__'
-
+    
+    def validate_publication_year(self, value):
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("The publication year cannot be in the future.")
+        return value
 
 class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    
     class Meta:
         name = Author
-        fields = ["", ""]
+        fields = ["name", "books"]
