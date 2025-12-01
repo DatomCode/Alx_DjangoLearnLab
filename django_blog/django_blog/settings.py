652ab2bd0aec16fe888d09f49a09e9bf0f58b511
Datom-code <enochgbadebo007@gmail.com>
2025-12-02 00:33:32 +0100

    configure PostgreSQl db to use for the blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        #PostgreSQL configuration
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_blog_db',    
+        'USER': 'postgres',        
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',   
+        'PORT': '5432',
     }
 }
 
